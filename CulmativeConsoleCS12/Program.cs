15a384b107ff4c4983d397d4767390af01a6820f
jsuurman@gmail.com <jsuurman@gmail.com>
2024-04-10 13:49:02 -0400

    Updated 04/10

--- CulmativeConsoleCS12/Program.cs
+++ CulmativeConsoleCS12/Program.cs
@@ -17,7 +17,7 @@ namespace CulmativeConsoleCS12
             string connectionString = "HostName=WCHS.azure-devices.net;DeviceId=wchs6;SharedAccessKey=5qXtc9bX4D66hoqSviZVXEMhoK/SVPCvOAIoTAVQHns=";
             try
             {
-                SerialPort port = new SerialPort{BaudRate = 9600, PortName = "COM3"};
+                SerialPort port = new SerialPort{BaudRate = 9600, PortName = "COM6"};
                 port.Open();
                 try
                 {
@@ -32,14 +32,14 @@ namespace CulmativeConsoleCS12
                 catch (Exception ex) {
                     Console.WriteLine("Encountered error while reading serial port");
                     Console.WriteLine(ex.ToString());
-                    System.Threading.Thread.Sleep(50000);
+                    System.Threading.Thread.Sleep(500);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Encountered error while opening serial port ");
                 Console.WriteLine(ex.ToString());
-                System.Threading.Thread.Sleep(50000);
+                System.Threading.Thread.Sleep(500);
             }
         }
         public async static void SendMsg(string msg)
@@ -48,7 +48,7 @@ namespace CulmativeConsoleCS12
             Message message = new Message(Encoding.ASCII.GetBytes(jsonData));
             await deviceClient.SendEventAsync(message);
             Console.WriteLine("{0:G} > Sent msg: {1}", DateTime.Now, msg);
-            await Task.Delay(3000);
+            await Task.Delay(300);
         }
     }
 }
